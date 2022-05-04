df21f6958f384fae1e607bd1c6fc8d0f37bfc744
Griffin Lundquist <griffin@arcadiaitpartners.com>
2022-05-04 10:09:22 -0500

    file restructer

--- controllers/api/edamamRoutes.js
+++ controllers/api/edamamRoutes.js
@@ -4,7 +4,7 @@ const router = require('express').Router();
 const axios = require('axios').default;
 require('dotenv').config();
 
-router.get('/:searchterm', async (req, res) => {
+router.get('/searchterm', async (req, res) => {
     var queryURL = `https://api.edamam.com/search?q=${req.params.searchterm}&app_id=${process.env.APP_ID}&app_key=${process.env.APIKEY}&imageSize=THUMBNAIL&to=10`;
     // console.log(queryURL);
     axios.get(queryURL)
--- public/js/addRecipe.js
+++ public/js/addRecipe.js
@@ -1,5 +1,6 @@
 var submitBtn = $("#search-btn");
 var searchTerm = $("#keyword");
+//already recording value so may not need to fetch
 
 var searchResults = $("#recipeResults");
 
@@ -10,16 +11,23 @@ submitBtn.on("click", function (event) {
    searchTerm = searchTerm.val();
    
    
-   fetch('searchTerm', {
+   //fetch('searchTerm', {
+    //search term already being recording in variable
+    //await fetch
+    //stringify method
+    //set value to something
+    //call search term
+    //call api and do post method then give search term
+
     //.then((res) => res.json())
     //.then((data) => {
 })
 .catch((error) => {
   console.error('Error:', error);
 
-//add fetch on '/api/edamam/:searchterm
-//    recipeAPI(searchTerm)
-}); 
+ //add fetch on '/api/edamam/:searchterm
+ //    recipeAPI(searchTerm)
+})
 
 
 //var userSearchTerm = searchTerm.val();
@@ -27,7 +35,7 @@ submitBtn.on("click", function (event) {
 // function recipeAPI(searchTerm){
 
 //     var queryURL = "https://api.edamam.com/search?q=" & searchTerm & app_id & APIKey & "imageSize=thumbnail" & "to=10";
-
+//     call it and put search term in to it
 //     fetch(queryURL)
 //        .then(function (response) {
 //             return response.json();
