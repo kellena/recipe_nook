f4fc5fc132d1919a081d437f848985ad24a144f7
klmitchy <karenapolis@gmail.com>
2022-05-04 15:00:26 -0500

    fix

--- controllers/api/edamamRoutes.js
+++ controllers/api/edamamRoutes.js
@@ -41,11 +41,11 @@ router.get('/:searchterm', async (req, res) => {
         })
 
     })
-    .catch(function (error) {
-        // handle error
-        "oh fork, something went wrong"
-        console.log(error);
-    })
+    // .catch(function (error) {
+    //     // handle error
+    //     "oh fork, something went wrong"
+    //     console.log(error);
+    // })
 
 
 
